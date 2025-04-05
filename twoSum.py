9521c23abdf3eba90d06f8be9e26d127310cf0d5
maggibansal-tech <maggi.bansal30@gmail.com>
2025-04-05 13:27:40 +0530

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,15 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)<2 or len(nums)>10e4:
-            return['list too small or too large'] 
-        if target < -10e9 or target > 10e9: 
-            return['target too small or large']
-        for i in nums:
-            if i < -10e9 or i> 10e9:
-                return['values too small or too large']
-        l=[]
-        for i, x in enumerate(nums):
-            for j in range(i+1,len(nums)):
-                if x+nums[j]==target:
-                    l.extend([i,j])
-        return l
+        hashmap={}
+        for ind,i in enumerate(nums):
+            hashmap[i]=ind
+        for j in range(len(nums)):
+            diff=target-nums[j]
+            if diff in hashmap and hashmap[diff]!=j:
+                return [j, hashmap[diff]]
+        return []
